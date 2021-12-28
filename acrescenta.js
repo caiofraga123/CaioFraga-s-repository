81c0c6314049a7657c0ab71be096a01619ea3a27
caiofraga123 <92186508+caiofraga123@users.noreply.github.com>
2021-12-28 12:06:18 -0300

    Create acrescenta.js

---
+++ acrescenta.js
@@ -0,0 +1,13 @@
+function Altera() {
+	const materias = document.querySelectorAll('h2');
+		const pontos = new Array();
+		materias.forEach(materia => {
+			pontos.push(materia.textContent);
+		});
+		l = document.querySelector('ol');
+		pontos.forEach(ponto => {
+			const ut = document.createElement('li');
+			ut.textContent = ponto;
+			l.append(ut);
+		});
+	}
