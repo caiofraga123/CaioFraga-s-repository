43176172a05f270fd2185c5f8a1f1df5f9061c48
caiofraga123 <92186508+caiofraga123@users.noreply.github.com>
2021-12-28 22:20:27 -0300

    Update acrescenta.js

--- acrescenta.js
+++ acrescenta.js
@@ -1,11 +1,21 @@
 	const materias = document.querySelectorAll('h2');
-		const pontos = new Array();
-		materias.forEach(materia => {
-			pontos.push(materia.textContent);
+	const pontos = new Array();
+	materias.forEach(element => {
+		pontos.push(element.textContent);
+		const lk = document.createElement('a');
+		lk.setAttribute('name', element.textContent);
+		element.append(lk);
+		const lkr = document.createElement('a');
+		lkr.setAttribute('href' , '#');
+		lkr.textContent = 'retorno';
+		element.parentElement.insertBefore(lkr, element.nextElementSibling);
 		});
-		l = document.querySelector('ol');
-		pontos.forEach(ponto => {
-			const ut = document.createElement('li');
-			ut.textContent = ponto;
-			l.append(ut);
-		});
+	l = document.querySelector('ol');
+	pontos.forEach(ponto => {
+		const ut = document.createElement('li');
+		l.append(ut);
+		const lkn = document.createElement('a');
+		lkn.setAttribute('href', '#${ponto}');
+		lkn.textContent = ponto;
+		ut.append(lkn);
+	});
