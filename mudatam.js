8ec3f913b1a7e33ce6e51be23802b0ae1df1dec9
caiofraga123 <92186508+caiofraga123@users.noreply.github.com>
2022-01-06 14:41:10 -0300

    Update mudatam.js

--- mudatam.js
+++ mudatam.js
@@ -2,24 +2,24 @@ document.addEventListener("DOMContentLoaded", selec);
 
 let num = 1;
 
-let tamanho = 2;
+let tam = 3;
 
-    function atualizatamanho() {
-        const h1 = document.querySelector(`h${num}`);
-        h1.style.fontSize = `${tamanho}em`;
+    function mudatam() {
+        const e = document.querySelector(`h${num}`);
+        e.style.fontSize = `${tam}em`;
     }
 	
-    function aumentatitulo() {
-		if (tamanho < 9) {
-			tamanho += 0.5;
-			atualizatamanho()
+    function aumenta() {
+		if (tam < 10) {
+			tam += 0.5;
+			mudatam()
 		}
     }
 	
-    function diminuititulo() {
-        if (tamanho > 1) {
-			tamanho -= 0.5;
-            atualizatamanho();
+    function diminui() {
+        if (tam > 1) {
+			tam -= 0.5;
+            mudatam();
         }
 	}
 	
@@ -27,14 +27,14 @@ function cria () {
 	botao = document.createElement("button")
 	botao.innerHTML = "+"
 	botao.addEventListener("click", function () {
-		aumentatitulo();
+		aumenta();
 	});
 	a = document.querySelector(`h${num}`)
 	a.after(botao);
 	bota = document.createElement("button")
 	bota.innerHTML = "-"
 	bota.addEventListener("click", function () {
-		diminuititulo();
+		diminui();
 	});
 	b = document.querySelector(`h${num}`)
 	b.after(bota);
